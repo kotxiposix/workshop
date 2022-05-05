b20d32c1ad2ef857bf37e3c67fe0654c42d482b3
anilson <anilsonmonteiro@gmail.com>
2022-05-05 19:45:31 -0100

    add Flutter workshop materials

---
+++ Flutter - Fundamentals and Deep Dive/01_startup_namer/part_1/startup_namer/lib/main.dart
@@ -0,0 +1,57 @@
+import 'package:english_words/english_words.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const HelloApp());
+}
+
+class HelloApp extends StatelessWidget {
+  const HelloApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Startup Name Generator",
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Center(child: Text("Startup Name Generator")),
+        ),
+        body: const Center(
+          child: RandomWords(),
+        ),
+      ),
+    );
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  const RandomWords({Key? key}) : super(key: key);
+
+  @override
+  State<RandomWords> createState() => _RandomWordsState();
+}
+
+class _RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];
+  final _biggerFont = const TextStyle(fontSize: 18);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      padding: const EdgeInsets.all(16.0),
+      itemBuilder: (context, i) {
+        if (i.isOdd) return const Divider();
+        final index = i ~/ 2;
+        if (index >= _suggestions.length) {
+          _suggestions.addAll(generateWordPairs().take(10));
+        }
+        return ListTile(
+          title: Text(
+            _suggestions[index].asPascalCase,
+            style: _biggerFont,
+          ),
+        );
+      },
+    );
+  }
+}
---
+++ Flutter - Fundamentals and Deep Dive/labs/dart_oop_quick_tour/README.md
@@ -0,0 +1,1 @@
+# Dart & OOP - Quick Tour
